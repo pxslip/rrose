bf83f1f0c3ddeefbebd88431e1a5008482ef892f
Will Kruse <wskruse@gmail.com>
2018-07-02 12:48:23 -0400

    Make x_bound and y_bound settable from options, bump to 0.2.1

--- leaflet.rrose-src.js
+++ leaflet.rrose-src.js
@@ -1,23 +1,23 @@
 /*
   Copyright (c) 2012 Eric S. Theise
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
-  documentation files (the "Software"), to deal in the Software without restriction, including without limitation the 
-  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit 
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+  documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
   persons to whom the Software is furnished to do so, subject to the following conditions:
-  
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the 
+
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
   Software.
-  
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
-  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
-  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
   OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-    
+
 L.Rrose = L.Popup.extend({
 
-  _initLayout:function () {
+  _initLayout: function () {
     var prefix = 'leaflet-rrose',
       container = this._container = L.DomUtil.create('div', prefix + ' ' + this.options.className + ' leaflet-zoom-animated'),
       closeButton, wrapper;
@@ -31,7 +31,8 @@ L.Rrose = L.Popup.extend({
     }
 
     // Set the pixel distances from the map edges at which popups are too close and need to be re-oriented.
-    var x_bound = 80, y_bound = 80;
+    var x_bound = (typeof this.options.x_bound === 'undefined') ? 80 : this.options.x_bound;
+    var y_bound = (typeof this.options.y_bound === 'undefined') ? 80 : this.options.y_bound;
     // Determine the alternate direction to pop up; north mimics Leaflet's default behavior, so we initialize to that.
     this.options.position = 'n';
     // Then see if the point is too far north...
@@ -44,7 +45,7 @@ L.Rrose = L.Popup.extend({
     if (x_diff > 0) {
       this.options.position += 'w'
     } else {
-    // or too far west.
+      // or too far west.
       x_diff = x_bound - this._map.latLngToContainerPoint(this._latlng).x;
       if (x_diff > 0) {
         this.options.position += 'e'
@@ -56,8 +57,7 @@ L.Rrose = L.Popup.extend({
       if (this.options.position === 's') {
         this._tipContainer = L.DomUtil.create('div', prefix + '-tip-container', container);
         wrapper = this._wrapper = L.DomUtil.create('div', prefix + '-content-wrapper', container);
-      } 
-      else {
+      } else {
         this._tipContainer = L.DomUtil.create('div', prefix + '-tip-container' + ' ' + prefix + '-tip-container-' + this.options.position, container);
         wrapper = this._wrapper = L.DomUtil.create('div', prefix + '-content-wrapper' + ' ' + prefix + '-content-wrapper-' + this.options.position, container);
       }
@@ -65,13 +65,11 @@ L.Rrose = L.Popup.extend({
       L.DomEvent.disableClickPropagation(wrapper);
       this._contentNode = L.DomUtil.create('div', prefix + '-content', wrapper);
       L.DomEvent.on(this._contentNode, 'mousewheel', L.DomEvent.stopPropagation);
-    } 
-    else {
+    } else {
       if (this.options.position === 'n') {
         wrapper = this._wrapper = L.DomUtil.create('div', prefix + '-content-wrapper', container);
         this._tipContainer = L.DomUtil.create('div', prefix + '-tip-container', container);
-      } 
-      else {
+      } else {
         wrapper = this._wrapper = L.DomUtil.create('div', prefix + '-content-wrapper' + ' ' + prefix + '-content-wrapper-' + this.options.position, container);
         this._tipContainer = L.DomUtil.create('div', prefix + '-tip-container' + ' ' + prefix + '-tip-container-' + this.options.position, container);
       }
@@ -83,7 +81,7 @@ L.Rrose = L.Popup.extend({
 
   },
 
-  _updatePosition:function () {
+  _updatePosition: function () {
     var pos = this._map.latLngToLayerPoint(this._latlng),
       is3d = L.Browser.any3d,
       offset = this.options.offset;
@@ -100,11 +98,9 @@ L.Rrose = L.Popup.extend({
 
     if (/e/.test(this.options.position)) {
       this._containerLeft = offset.x + (is3d ? 0 : pos.x);
-    } 
-    else if (/w/.test(this.options.position)) {
+    } else if (/w/.test(this.options.position)) {
       this._containerLeft = -Math.round(this._containerWidth) + offset.x + (is3d ? 0 : pos.x);
-    } 
-    else {
+    } else {
       this._containerLeft = -Math.round(this._containerWidth / 2) + offset.x + (is3d ? 0 : pos.x);
     }
 
